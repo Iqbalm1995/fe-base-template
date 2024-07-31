895f69f89b77df1babd3e874544f059b77a28870
Mohamad Iqbal Musyaffa <36550193+Iqbalm1995@users.noreply.github.com>
2024-07-31 15:40:23 +0700

    update src

---
+++ src/types/dummyJsonData.ts
@@ -0,0 +1,55 @@
+export interface StaticMonthDataTypes {
+    monthNumber : string;
+    monthName : string;
+}
+
+export const StaticMonthData: StaticMonthDataTypes[] = [
+  {
+    monthNumber: "01",
+    monthName: "Januari",
+  },
+  {
+    monthNumber: "02",
+    monthName: "Februari",
+  },
+  {
+    monthNumber: "03",
+    monthName: "Maret",
+  },
+  {
+    monthNumber: "04",
+    monthName: "April",
+  },
+  {
+    monthNumber: "05",
+    monthName: "Mei",
+  },
+  {
+    monthNumber: "06",
+    monthName: "Juni",
+  },
+  {
+    monthNumber: "07",
+    monthName: "Juli",
+  },
+  {
+    monthNumber: "08",
+    monthName: "Agustus",
+  },
+  {
+    monthNumber: "09",
+    monthName: "September",
+  },
+  {
+    monthNumber: "10",
+    monthName: "Oktober",
+  },
+  {
+    monthNumber: "11",
+    monthName: "November",
+  },
+  {
+    monthNumber: "12",
+    monthName: "Desember",
+  },
+];
